6bc99ac2bb75b495440da18277d30b48c807c728
diegofdg <diegofdg@gmail.com>
2022-01-29 08:16:31 -0300

    agregar angularfire2

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
+  firebase: {
+    projectId: 'mastergym-98ebd',
+    appId: '1:471524482051:web:74d987dabd14e293d1c236',
+    storageBucket: 'mastergym-98ebd.appspot.com',
+    apiKey: 'AIzaSyC3pt6FRCLITugaUzHxbamhN09Lqvt2oXE',
+    authDomain: 'mastergym-98ebd.firebaseapp.com',
+    messagingSenderId: '471524482051',
+    measurementId: 'G-T9P457D43E',
+  },
   production: true
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    projectId: 'mastergym-98ebd',
+    appId: '1:471524482051:web:74d987dabd14e293d1c236',
+    storageBucket: 'mastergym-98ebd.appspot.com',
+    apiKey: 'AIzaSyC3pt6FRCLITugaUzHxbamhN09Lqvt2oXE',
+    authDomain: 'mastergym-98ebd.firebaseapp.com',
+    messagingSenderId: '471524482051',
+    measurementId: 'G-T9P457D43E',
+  }  
 };
 
 /*
