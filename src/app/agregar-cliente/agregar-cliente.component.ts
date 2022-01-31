96c856ee158038acd913343188665c1c399bd195
diegofdg <diegofdg@gmail.com>
2022-01-30 22:03:55 -0300

    registrar cliente en firebase

--- src/app/agregar-cliente/agregar-cliente.component.ts
+++ src/app/agregar-cliente/agregar-cliente.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { AngularFireStorage } from '@angular/fire/compat/storage';
+import { AngularFirestore, AngularFirestoreCollection } from '@angular/fire/compat/firestore';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
@@ -9,9 +10,10 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 })
 export class AgregarClienteComponent implements OnInit {
   formularioCliente: FormGroup = new FormGroup({});
-  porcentajeSubida: Number = 0;  
+  porcentajeSubida: Number = 0;
+  urlImagen: string = '';
   
-  constructor(private fb: FormBuilder, private storage: AngularFireStorage) { }
+  constructor(private fb: FormBuilder, private storage: AngularFireStorage, private afs: AngularFirestore) { }
 
   ngOnInit(): void {
     this.formularioCliente = this.fb.group({
@@ -28,33 +30,40 @@ export class AgregarClienteComponent implements OnInit {
   }
 
   agregar() {
-    console.log(this.formularioCliente.value);    
+    this.formularioCliente.value.imgUrl = this.urlImagen;
+    this.formularioCliente.value.fechaNacimiento = new Date(this.formularioCliente.value.fechaNacimiento );
+    console.log(this.formularioCliente.value)
+    this.afs.collection('clientes').add(this.formularioCliente.value).then((termino)=>{
+      console.log('registro creado');      
+    });
   }
 
   subirImagen(event:any) {
-    let nombre = new Date().getTime().toString();
-    let archivo = event.target.files[0];
-    let extension =  archivo.name.toString().substring(archivo.name.toString().lastIndexOf('.'))    
-    const ruta = 'clientes/' + nombre  + extension ;
-    const referencia = this.storage.ref(ruta);
-    const tarea = referencia.put(archivo);
-
-    tarea.then((objeto) => {
-      console.log(objeto);
-      console.log('imagen subida correctamente');      
-      
-      referencia.getDownloadURL().subscribe((url) => {
-        console.log(url);
+    if(event.target.files.length > 0) {      
+      let nombre = new Date().getTime().toString();
+      let archivo = event.target.files[0];
+      let extension =  archivo.name.toString().substring(archivo.name.toString().lastIndexOf('.'))    
+      const ruta = 'clientes/' + nombre  + extension ;
+      const referencia = this.storage.ref(ruta);
+      const tarea = referencia.put(archivo);
+  
+      tarea.then((objeto) => {
+        console.log(objeto);
+        console.log('imagen subida correctamente');      
+        
+        referencia.getDownloadURL().subscribe((url) => {
+          this.urlImagen = url;
+        });
       });
-    });
-
-    tarea.percentageChanges().subscribe((porcentaje) => {
-      if(porcentaje !== undefined) {
-        this.porcentajeSubida = parseInt( porcentaje.toString());
-        const barra:any = document.getElementById('barra');
-        barra.style.width = this.porcentajeSubida + '%';
-        barra.setAttribute('aria-valuenow', this.porcentajeSubida);
-      }
-    });
+  
+      tarea.percentageChanges().subscribe((porcentaje) => {
+        if(porcentaje !== undefined) {
+          this.porcentajeSubida = parseInt( porcentaje.toString());
+          const barra:any = document.getElementById('barra');
+          barra.style.width = this.porcentajeSubida + '%';
+          barra.setAttribute('aria-valuenow', this.porcentajeSubida);
+        }
+      });
+    }
   }
 }
