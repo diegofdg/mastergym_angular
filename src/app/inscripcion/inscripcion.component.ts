24ffa674fa29e09010be57ad174dff05f60a6b4f
diegofdg <diegofdg@gmail.com>
2022-01-31 20:50:45 -0300

    seleccionar días

--- src/app/inscripcion/inscripcion.component.ts
+++ src/app/inscripcion/inscripcion.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/compat/firestore';
 import { Cliente } from '../models/Cliente';
 import { Inscripcion } from '../models/Inscripcion';
+import { Precio } from '../models/Precio';
 
 @Component({
   selector: 'app-inscripcion',
@@ -11,10 +13,20 @@ export class InscripcionComponent implements OnInit {
   inscripcion: Inscripcion = new Inscripcion();
   clienteSeleccionado: Cliente = new Cliente();
   nombre: undefined;
+  precios: Precio[] = new Array< Precio>();
 
-  constructor() { }
+  constructor(private db: AngularFirestore) { }
 
-  ngOnInit(): void { }
+  ngOnInit(): void {
+    this.db.collection<Precio>('precios').get().subscribe((resultado)=>{
+      resultado.docs.forEach((item)=>{
+        let precio:any = item.data() as Precio;
+        precio.id = item.id;
+        precio.ref = item.ref;
+        this.precios.push(precio)
+      })
+    })
+  }
 
   asignarCliente(cliente: Cliente) {
     this.inscripcion.cliente = cliente.ref;
@@ -25,7 +37,7 @@ export class InscripcionComponent implements OnInit {
     this.clienteSeleccionado = new Cliente();
     this.inscripcion.cliente = undefined;
   }
-  
+
   guardar() {
     console.log(this.inscripcion);    
   }
